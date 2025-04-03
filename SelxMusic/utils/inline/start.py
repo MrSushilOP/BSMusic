da2fff32a716bd1c17e1c598540a32c545b8ff80
Sushil Maurya <99750148+MrSushilOP@users.noreply.github.com>
2025-04-03 08:58:00 +0530

    Update start.py

--- SelxMusic/utils/inline/start.py
+++ SelxMusic/utils/inline/start.py
@@ -30,8 +30,8 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], user_id=config.CO_OWNER_ID),
+           # InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+           # InlineKeyboardButton(text=_["S_B_7"], user_id=config.CO_OWNER_ID),
         ],
     ]
     return buttons
